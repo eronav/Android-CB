dc936627495adf26a6183688a05daa11814c744d
Ronav <mailronv@gmail.com>
2020-04-12 16:51:30 -0700

    play again intent attempt 1

---
+++ app/src/main/java/com/example/cb/secondGameActivity.java
@@ -0,0 +1,13 @@
+package com.example.cb;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class secondGameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second_game);
+    }
+}
