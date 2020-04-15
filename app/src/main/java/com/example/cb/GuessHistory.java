3b42b620bee537639a7b57a6488e7be11e3206f4
Ronav <mailronv@gmail.com>
2020-04-14 21:57:22 -0700

    refacter code into classes evaluation adds to list view

---
+++ app/src/main/java/com/example/cb/GuessHistory.java
@@ -0,0 +1,74 @@
+package com.example.cb;
+
+import android.content.Context;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GuessHistory {
+    ListView listBox;
+    List<String> List_file;
+    ArrayAdapter<String> listAdapter;
+    Context appcontext;
+
+    GuessHistory() {
+        List_file = new ArrayList<String>();
+    }
+
+    public void SetContext(Context ctxt) {
+    }
+
+    public void SetListBox(ListView lbox) {
+    }
+
+    public void CreateListView(Context ctxt, ListView lbox) {
+        //List_file.add("Apple");
+        //Create an adapter for the listView and add the ArrayList to the adapter.
+
+        appcontext = ctxt;
+        listBox = lbox;
+
+        // List_file.add(0, "Entry1");
+        // List_file.add(0, "Entry2");
+
+        listAdapter = new ArrayAdapter<String>(appcontext, android.R.layout.simple_list_item_1, List_file);
+        listBox.setAdapter(listAdapter);
+        listAdapter.notifyDataSetChanged();
+
+        listBox.setOnItemClickListener(new AdapterView.OnItemClickListener()
+        {
+            @Override
+            public void onItemClick(AdapterView<?> arg0, View arg1, int arg2, long arg3)
+            {
+                //args2 is the listViews Selected index
+            }
+        });
+    }
+
+    public void AddEntry(WordGenerator wgen, String guess) {
+        String errmsg;
+        int[] result = wgen.evaluateGuess(guess);
+        int attempts = List_file.size() + 1;
+
+        // ((game_screen)appcontext).setDebugBox(sz_a + "." + sz_b + "." + sz_c);
+        if (result[0] == 0) {
+            int cows, bulls;
+
+            cows = result[1]; bulls = result[2];
+            errmsg = ("Your word has " + bulls + " bulls and " + cows + " cows. Please guess again");
+            String stringCow = String.valueOf(cows);
+            String stringBull = String.valueOf(bulls);
+            List_file.add(0, guess + ": " + stringCow + "C " + stringBull + "B: guess " + String.valueOf(attempts));
+        } else if (result[0] == 1) {
+            List_file.add(0, guess + ": Correct guess in: " + String.valueOf(attempts) + " guesses!");
+        } else {
+            errmsg = ("Incompatible guess");
+        }
+
+        listAdapter.notifyDataSetChanged();
+    }
+}
--- app/src/main/java/com/example/cb/game_screen.java
+++ app/src/main/java/com/example/cb/game_screen.java
@@ -1,19 +1,44 @@
 package com.example.cb;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-import android.widget.TextView;
+import java.util.Random;
 
 public class game_screen extends AppCompatActivity {
 
+    ListView histbox;
+    EditText guess_box;
+    Button done_btn;
+    TextView testing_dash;
+    Random myrand;
+    WordGenerator wgen;
+    GuessHistory ghist;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        //Create the list View
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_screen);
+
+        histbox = findViewById(R.id.history);
+        guess_box = (EditText) findViewById(R.id.guess_box);
+        done_btn = (Button) findViewById(R.id.done_btn);
+        testing_dash = (TextView) findViewById(R.id.word);
+        myrand= new Random();
+
         int diff;
         String dash = "";
-        TextView testing_dash = (TextView) findViewById(R.id.word);
+        int i = 0;
         if(getIntent().hasExtra("com.mailronav.cb.three")){
             diff = 3;
         } else if(getIntent().hasExtra("com.mailronav.cb.four")){
@@ -23,9 +48,35 @@ public class game_screen extends AppCompatActivity {
         } else {
             diff = 6;
         }
-        for(int i = 0; i < diff; i++){
+        /*for(int i = 0; i < diff; i++){
             dash += "-";
         }
-        testing_dash.setText(dash);
+        testing_dash.setText(dash);*/
+
+        wgen = new WordGenerator(diff);
+        String word = wgen.getWord();
+
+        ghist = new GuessHistory();
+        ghist.CreateListView(game_screen.this, histbox);
+
+
+        done_btn.setOnClickListener(new View.OnClickListener() {
+            String guess = "";
+
+            @Override
+            public void onClick(View v) {
+                guess = String.valueOf(guess_box.getText());
+                ghist.AddEntry(wgen, guess);
+            }
+        });
+        testing_dash.setText(word);
+
+
     }
+    //Create the creation method
+
+    public void setDebugBox(String s) {
+        testing_dash.setText(s);
+    }
+
 }
